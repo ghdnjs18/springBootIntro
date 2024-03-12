72ddf26a861be1d25f22bd154c583d6ceecba682
hoho <ghdnjs18@naver.com>
2024-03-12 17:52:07 +0900

    member repository 생성

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 공유되는 변수일 경우 동시성 문제가 발생할 수 있어 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려한다.
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null이 반환될 가능성이 있으면 Optional을 사용해서 null 값을 잡아준다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 하나라도 찾으면 결과를 가져온다. 없으면 null
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
