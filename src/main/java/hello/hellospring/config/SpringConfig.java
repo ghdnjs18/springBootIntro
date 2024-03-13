1bcc187c706ebab4430b469ad3a58fc365715eaf
hoho <ghdnjs18@naver.com>
2024-03-13 19:57:35 +0900

    h2 데이터베이스 연결

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,18 +1,30 @@
 package hello.hellospring.config;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     // 자바 코드로 직접 스프링 빈 등록
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
