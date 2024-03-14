25aab6f3bb0c178aa01dc35c08789a3a4798e3cd
hoho <ghdnjs18@naver.com>
2024-03-14 22:04:08 +0900

    데이터베이스 JPA 적용

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,10 +1,9 @@
 package hello.hellospring.config;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,16 +15,22 @@ public class SpringConfig {
 
     // 자바 코드로 직접 스프링 빈 등록
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//    @PersistenceContext // 원래는 추가를 해주어야 한다.
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+//    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(EntityManager em) {
+//        this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
